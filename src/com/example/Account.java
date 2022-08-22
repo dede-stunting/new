55c05d17c9cdff62a74be662179e2cb77800f6fb
dede-stunting <107819420+dede-stunting@users.noreply.github.com>
2022-08-22 18:48:25 +0100

    secont test

---
+++ src/com/example/Account.java
@@ -0,0 +1,11 @@
+package com.example;
+import static java.lang.System.out;
+
+public class Account {
+
+
+        String name;
+        String address;
+        double balance;
+    }
+
---
+++ src/com/example/UseAccount.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import static java.lang.System.out;
+
+public class UseAccount {
+    public static void main(String[] args) {
+        Account firstAccount = new Account();
+        Account secondAccount = new Account();
+
+    //    out.println(yourAccount.name);
+        firstAccount.name = "Chiedu David";
+        firstAccount.address = "1, Nigerian Development Foundation";
+        firstAccount.balance = 724.70;
+        secondAccount.name = "Chiedu C. Daniel";
+        secondAccount.address = "Newton Lane";
+        secondAccount.balance = 100.00;
+        out.print(firstAccount.name);
+        out.print(" (");
+        out.print(firstAccount.address);
+        out.print(") has $");
+        out.print(firstAccount.balance);
+        out.println();
+        out.print(secondAccount.name);
+        out.print(" (");
+        out.print(secondAccount.address);
+        out.print(") has $");
+        out.print(secondAccount.balance);
+    }
+}
